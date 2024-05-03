ee48bcabdd7afa14f55a399efea6da7dd0de647f
baka.strumpf <baka.strumpf@gmail.com>
2024-05-03 22:57:14 +0200

    it finally works! able to create new users

--- src/main/java/com/iktpreobuka/validationtwo/controllers/UserController.java
+++ src/main/java/com/iktpreobuka/validationtwo/controllers/UserController.java
@@ -4,8 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-import javax.validation.Valid;
-
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
--- src/main/java/com/iktpreobuka/validationtwo/entities/UserEntity.java
+++ src/main/java/com/iktpreobuka/validationtwo/entities/UserEntity.java
@@ -1,17 +1,7 @@
 package com.iktpreobuka.validationtwo.entities;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Version;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Size;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
 
 @Entity
 public class UserEntity {
